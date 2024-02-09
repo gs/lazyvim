469c89b6824b4baefebe4489ce1cb371d3b7b730
gs <gs@deliverists.io>
2024-02-09 10:39:10 +0100

    update the <cr> as test runner

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,4 +4,4 @@
 --
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<c-p>", "<esc>:Telescope find_files<cr>")
-vim.keymap.set("n", "<cr>", "<esc>:w!|:vsp<cr>|:TestFile<cr>")
+vim.keymap.set("n", "<cr>", "<esc>:w!|:TestFile<cr>")
