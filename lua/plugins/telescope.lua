0a6b91d8cdf91349db9e22d5df1574a7a020439b
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2024-04-09 13:12:12 +0200

    Update of lazyvim + telescope vertical

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "telescope.nvim",
+    opts = {
+      defaults = {
+        -- https://www.reddit.com/r/neovim/comments/r22xrq/comment/hm2dv20/
+        layout_strategy = "vertical",
+        layout_config = {
+          height = 0.95,
+        },
+      },
+    },
+  },
+}
