452d222e96a0846d81ba403e21e344cc622b3486
gs <gs@deliverists.io>
2024-04-26 16:14:58 +0200

    Disable lazysnippets and extend ruby with rails and add html to erb

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("luasnip").filetype_extend("ruby", { "rails" })
+require("luasnip").filetype_extend("eruby", { "html" })
