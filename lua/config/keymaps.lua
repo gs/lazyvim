c3e0ae9339b992eb95a8adec9450dd444a985063
Grzegorz Smajdor <grzegorz@yelp.com>
2024-04-29 10:40:06 +0200

    Updates of the configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,5 +4,6 @@
 --
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<c-p>", "<esc>:Telescope find_files<cr>")
+vim.keymap.set("n", "<c-b>", "<esc>:Telescope buffers<cr>")
 vim.keymap.set("n", "<cr>", "<esc>:w!|:TestFile<cr>")
 vim.keymap.set("n", "<c-l>", "<esc>:Neogit<cr>")
