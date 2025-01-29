ae4d6f7287dcee3a4d43c04f7d21f7ed3f009448
Grzegorz Smajdor <gs@deliverists.io>
2025-01-29 13:02:21 +0100

    Adding octo! and remap keybiding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,4 +4,4 @@
 --
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<cr>", "<esc>:w!|:TestFile<cr>")
-vim.keymap.set("n", "<c-l>", "<esc>:Neogit<cr>")
+vim.keymap.set("n", "<c-x>", "<esc>:Neogit<cr>")
