284d3674fc1f163081371284d6e486e949b506aa
Grzegorz Smajdor <grzegorz@yelp.com>
2025-04-07 20:39:15 +0200

    remove unused plugins (used with eXtras)

--- lua/plugins/harpoon.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'ThePrimeagen/harpoon',
-  config = function()
-    vim.api.nvim_set_keymap('n', ',a', ':lua require("harpoon.mark").add_file()<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', ',l', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true })
-  end,
-}
